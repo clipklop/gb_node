bf75df792ffb085a3ab8923a71bc2daddcd70064
clipklop <siliconchik@gmail.com>
2018-10-06 09:50:16 +0300

    completed version

--- hw2/headsAndTails.js
+++ hw2/headsAndTails.js
@@ -1,15 +1,16 @@
-//
+// Heads and Tails game on NodeJS. Two sides of the same coin.
 
 
-const rl = require('readline');
+const rl = require('readline').createInterface(process.stdin, process.stdout);
 
 
-function randInt() {
-  return Math.floor((Math.random() * 1) + 1);
+const randInt = function() {
+  return Math.round(Math.random());
 }
 
-rl.question('Heads and Tails. Two sides of the same coin.', answer => {
+rl.question('Heads or Tails. Please choose one side of the same coin: 1 or 0: ', (answer) => {
   let rand = parseInt(answer);
   // TODO logic
+  rand === randInt() ? console.log("Yay, Heads! You've won!") : console.log("Tails :( Better luck next time.");
   rl.close();
 });
